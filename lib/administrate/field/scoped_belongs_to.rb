045a29fdc1b39a805cea50830dfbc9b3485162c0
Finn Heemeyer <github@tonekk.de>
2023-10-29 14:34:58 +0100

    Add scoped_belongs_to field

---
+++ lib/administrate/field/scoped_belongs_to.rb
@@ -0,0 +1,36 @@
+require 'administrate/field/belongs_to'
+require 'administrate/field/scoped_has_many/version'
+require 'rails/engine'
+
+module Administrate
+  module Field
+    class ScopedBelongsTo < Administrate::Field::BelongsTo
+      include ScopedHasManyVersion
+
+      class Engine < ::Rails::Engine
+      end
+
+      def to_s
+        data
+      end
+
+      def to_partial_path
+        "/fields/#{self.class.superclass.field_type}/#{page}"
+      end
+
+      def self.html_class
+        "belongs-to"
+      end
+
+      private
+
+      def candidate_resources
+        scope = options[:scope] ? options[:scope].call(self) : associated_class.all
+        scope = scope.includes(*options.fetch(:includes)) if options.key?(:includes)
+
+        order = options.delete(:order)
+        order ? scope.reorder(order) : scope
+      end
+    end
+  end
+end
